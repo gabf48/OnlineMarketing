e2181077086f2453a88bfd6635de7dbbbc1bea99
Gabriel Filip <gabriel.filip@am-sys.com>
2023-05-15 18:49:52 +0300

    update project

--- migrare_document_koff.py
+++ migrare_document_koff.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('D:\OnlineMarketing\koff.csv', on_bad_lines='skip')
+df = pd.read_csv('C:\OnlineMarketing\koff_stocks.csv', on_bad_lines='skip')
 
 df.to_excel('Final_koff_prod.xlsx', index=False)
